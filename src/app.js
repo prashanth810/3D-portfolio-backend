9769545f979b2bebbff86c465b2a0d7530d9b4ae
Your Name <your-email@example.com>
2025-12-16 08:29:29 +0530

    cors changes done

--- src/app.js
+++ src/app.js
@@ -8,11 +8,7 @@ import mailroutes from "./routes/MailRoutes.js";
 
 const app = express();
 app.use(helmet());
-app.use(cors({
-    origin: "https://3dportfolio-blue-seven.vercel.app",
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-}));
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
